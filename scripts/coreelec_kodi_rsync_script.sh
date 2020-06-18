deefe253fc5edad7184c6b657025e11c74d5b2aa
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2020-06-18 22:08:01 +0700

    Update coreelec_kodi_rsync_script.sh

--- scripts/coreelec_kodi_rsync_script.sh
+++ scripts/coreelec_kodi_rsync_script.sh
@@ -53,7 +53,7 @@ if [ "$NAS_LINK" == 0 ] && [ "$NAS_PING" == 0 ] && [ "$MOUNT_CHECK" == 0 ]; then
   rsync -av -e "ssh -p$RSYNC_SSH_PORT" --progress  --human-readable --partial --delete --inplace --exclude '*.partial~' --delete-excluded --log-file=$LOGFILE --files-from=$INPUT_LIST --relative kodi_rsync@$NAS_IP:$USER_BASE_DIR $DESTINATION_DIR 2> $LOGFILE_ERRORS
   echo "==================================" >> $LOGFILE
   find $LOCAL_DIR -type f | sed "s#$DESTINATION_DIR#/#" > $DESTINATION_DIR\logs/local_removefilelist_var01
-  awk 'NR==FNR {exclude[$0];next} !($0 in exclude)' $INPUT_LIST $DESTINATION_DIR\logs/local_removelist_var01 | sed 's/^.//' | sed "s#^#$DESTINATION_DIR#" > $DESTINATION_DIR\logs/local_removefilelist_input
+  awk 'NR==FNR {exclude[$0];next} !($0 in exclude)' $INPUT_LIST $DESTINATION_DIR\logs/local_removefilelist_var01 | sed 's/^.//' | sed "s#^#$DESTINATION_DIR#" > $DESTINATION_DIR\logs/local_removefilelist_input
   xargs rm -rf <$DESTINATION_DIR\logs/local_removefilelist_input
   find $LOCAL_DIR depth -type d -delete 2>/dev/null
 else
